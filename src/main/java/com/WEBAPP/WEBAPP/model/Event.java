f93132cbfb463d61ff76ae1022c9a3eaf54f263d
sharqi12 <50443495+sharqi12@users.noreply.github.com>
2020-10-21 10:16:48 +0200

    Combined project with event crud

--- src/main/java/com/WEBAPP/WEBAPP/model/Event.java
+++ src/main/java/com/WEBAPP/WEBAPP/model/Event.java
@@ -30,28 +30,38 @@ public class Event {
     public long getId() {
         return id;
     }
+
     public void setId(long id) {
         this.id = id;
     }
-    public String getName() { return name;}
+
+    public String getName() {
+        return name;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getCity() {
         return city;
     }
+
     public void setCity(String city) {
         this.city = city;
     }
     public String getDate() {
         return date;
     }
+
     public void setDate(String date) {
         this.date = date;
     }
+
     public String getDescription() {
         return description;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
