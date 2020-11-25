006e8d622382b9ac22ab734bd57902c0a6c7bff5
sharqi12 <50443495+sharqi12@users.noreply.github.com>
2020-11-25 10:34:05 +0100

    Timetable order by begin time

--- src/main/java/com/WEBAPP/WEBAPP/repository/TimetableRepository.java
+++ src/main/java/com/WEBAPP/WEBAPP/repository/TimetableRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public interface TimetableRepository extends JpaRepository<Timetable, Long> {
 
-    @Query(value = "SELECT * FROM timetables WHERE id_events =:em",nativeQuery = true)
+    @Query(value = "SELECT * FROM timetables WHERE id_events =:em ORDER BY begin",nativeQuery = true)
     List<Timetable> findByEventId(@Param("em")Long id);
 
 
